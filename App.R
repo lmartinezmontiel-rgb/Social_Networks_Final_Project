a5eac89cb440a5a111000bef206507a868def5c7
lmartinezmontiel-rgb <lmartinezmontiel@middlebury.edu>
2026-05-07 02:39:32 -0400

    Add files via upload

--- App.R
+++ App.R
@@ -3,14 +3,62 @@
 
 library(shiny) 
 library(bslib)
-
 library(tidyverse)
 library(igraph)
 library(tidygraph)
 library(ggraph)
-
+library(ggplot2)
 library(visNetwork)
 
+#Creating graphs
+
+STD_Edges <- read_csv("~/Desktop/STD_Edges_Data_FIXED.csv")
+STD_Nodes<- read_csv("~/Desktop/STD_Nodes_Data_Actully_Fixed.csv")
+
+STD_Full <- graph_from_data_frame(d=STD_Edges, vertices = STD_Nodes, directed = FALSE)
+
+###################################################################################################
+STD_Full_Cluster <- as_tbl_graph(STD_Full)
+
+STDCluster_L <- STD_Full_Cluster |> activate(nodes) |> 
+  mutate(
+    cluster     = group_louvain(),
+    degree      = centrality_degree(),
+    betweenness = centrality_betweenness()
+  )
+
+
+Cluster<- STDCluster_L |>  activate(nodes) |> as_tibble()
+
+table(Cluster$cluster, Cluster$Gender)
+
+table(Cluster$cluster, Cluster$Wealth)
+
+ggraph(STDCluster_L, layout = "fr") +
+  geom_edge_link(alpha = .3) +
+  geom_node_point(aes(color = as.factor(cluster)), size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE, size = 3)+
+  labs(
+    color = "Cluster",
+    title = "Louvain Clustering of Social Network")+
+  theme_graph()
+
+###################################################################################################
+
+STDCluster_L |>
+  activate(nodes) |> mutate(degree = centrality_degree()) |>
+  ggraph(layout = "fr") +
+  geom_edge_link(alpha = .3, color = "black") +
+  geom_node_point(aes(size = degree, color = Wealth)) +
+  geom_node_text(aes(label = name), repel = TRUE, size = 3) +
+  theme_graph() +
+  labs(
+    title = "Degree Centrality/Wealth Network",
+    size = "Degree",
+    color = "Wealth"
+  )
+
+###################################################################################################
 
 
 # Section 2. Design the site in the UI section (US = User Interface). This is where we define how everything looks and 
@@ -21,140 +69,120 @@ ui <-fluidPage(
   titlePanel("Social Networks Final Project"),
   
   page_sidebar(
-    title = "Visualization 1", 
-    sidebar = sidebar ("Menu Options"), 
+    title = "Calculating interations between characters in the spanish Telenovela Soy Tu Duena",
+    
+    sidebar = sidebar(
+      selectInput(
+        inputId = "size_by",
+        label = "Size nodes by:",
+        choices = c("Degree" = "degree", "betweenness" = "betweenness"),
+        selected = "degree"
+      )
+    ),
+    
     card(
       card_header("In this final project I decied to calculate how many interactions each character has 
-                  with one another, in the spanish telenovela, Soy Tu Duena. "), "This telenovela has 142 episodes
-      which, is alot, however I decided to only take data from only from the first,last and every 20 episodes in between."), 
+                  with one another, in the spanish telenovela, Soy Tu Duena. "), 
+      "This telenovela has 142 episodes which, is alot, however I decided to only take data 
+      from only from the first,last and every 20 episodes in between."),
     
+    card(card_header("Visualization #1 - VisNetwork"),
+         visNetworkOutput("int_network", height = "600px")),
     
-    card(card_header("Visualization 1"),
-         selectInput("size",
-                     "Choose Your Character", 
-                     choices = list("Character" = "???????degree", 
-                                    "Delte this?" = "betweenness"), 
-                     selected = 1), 
-         plotOutput("example_network"), height = "400px"),
+    card(card_header("Visualization #2 - Degree Centrality & Wealth"),
+         plotOutput("degree_plot", height = "600px")),
     
+    card(card_header("Visualization #3 - Louvain Clustering"),
+         plotOutput("cluster_plot", height = "600px"))
     
-    card(card_header("An interactive network?!"), 
-         "we can use the package VisNetwork to make it happen", 
-         radioButtons("size_by", "Centrality Measure", 
-                      choices = c("Degree" = "degree", 
-                      "Betweenness Centrality" = "betweenness"), 
-         selected = "degree"),
-         visNetworkOutput("int_network"), height = "600px")
-    )
   )
+)
 
 
 # Section 2. The server section defines how our app works. Here's where we will put all the network analysis. 
 
 server <- function(input, output) {
   
-  # CARD 1 
-  
-  output$ourVariable <- renderText({
-    paste("Our selected option is", input$select)
+  #card 1
+  output$cluster_plot <- renderPlot({
+    ggraph(STDCluster_L, layout = "fr") +
+      geom_edge_link(alpha = .3) +
+      geom_node_point(aes(color = as.factor(cluster)), size = 5) +
+      geom_node_text(aes(label = name), repel = TRUE, size = 3) +
+      labs(
+        color = "Cluster",
+        title = "Louvain Clustering of Social Network") +
+      theme_graph()
   })
   
-# let's create a simple example network with 10 nodes and calulate the degree centrality
-
-  # CARD 2 
+  #card 2
+  output$degree_plot <- renderPlot({
+    STDCluster_L |>
+      activate(nodes) |> mutate(degree = centrality_degree()) |>
+      ggraph(layout = "fr") +
+      geom_edge_link(alpha = .3, color = "black") +
+      geom_node_point(aes(size = degree, color = Wealth)) +
+      geom_node_text(aes(label = name), repel = TRUE, size = 3) +
+      theme_graph() +
+      labs(
+        title = "Degree Centrality/Wealth Network",
+        size = "Degree",
+        color = "Wealth"
+      )
+  })
   
-network <- reactive({
-  ex_net <- play_gnp(n = 10, p = 0.15, directed = FALSE)
-  
-  ex_net <- ex_net |> 
-    as_tbl_graph()|> 
-    activate(nodes) |> 
-    mutate(
-      degree = centrality_degree(), 
-      betweenness = centrality_betweenness())
-  
-  ex_net
-})
-
-# now let's get it visualized and reactive to our choice from above! 
-
-output$example_network <- renderPlot({
-  ex_net <- network() 
-  
-  p<- ggraph(ex_net, layout = "auto") +
-    geom_edge_link(alpha = 0.3, color = "grey80") + 
-    geom_node_point(aes(size = .data[[input$size]]),
-                    color = "pink") + 
-    scale_size_continuous(range = c(.5, 10)) + 
-    labs(Nodes = input$size) + 
-    theme_graph()
-  
-  p
-})
-
-# CARD 3 
-
-# we're going to use another example network like from above but visNetwork requires separate edge and nodes lists 
-
-network2 <- reactive({
-  set.seed(123)
-  ex_net2 <- play_gnp(n = 15, p = 0.25, directed = FALSE)
-  
-  ex_net2 <- ex_net2 |> 
-    as_tbl_graph()|> 
-    activate(nodes) |> 
-    mutate(
-      degree = centrality_degree(), 
-      betweenness = centrality_betweenness())
-  
-  nodes_df <- ex_net2 |> 
-    activate(nodes) |> 
-    as_tibble() |> 
-    rowid_to_column("id") |> 
-    mutate(value = if (input$size_by == "degree") degree else betweenness) # have to give size based on "value" for visNetwork
-  
-  edges_df <- ex_net2 |> 
-    activate(edges) |> 
-    as_tibble() |> 
-    rename(from = 1, to =2 )
-  
-  list(nodes = nodes_df, edges = edges_df)
-})
-
-output$int_network <- renderVisNetwork({
-   net2 <- network2()
-   nodes <- net2$nodes
-   edges <- net2$edges 
-  
-   
-  visNetwork(nodes, net2$edges) |> 
+  #Card 3
+  network2 <- reactive({
     
-    visNodes(borderWidth = 1, 
-             color = list(
-               background= "pink", 
-               border = "red", 
-               highlight =  "purple"))|>
+    nodes_df <- STDCluster_L |>
+      activate(nodes) |>
+      as_tibble() |>
+      mutate(
+        id = row_number(),
+        label = name,
+        value = ifelse(input$size_by == "degree", degree, betweenness),
+        group = cluster
+      )
     
-    visEdges(
-      color = list(color = "purple", highlight = "black")) |> 
+    edges_df <- STDCluster_L |>
+      activate(edges) |>
+      as_tibble() |>
+      rename(from = from, to = to)
     
-    visOptions(
-      highlightNearest = list(enabled = TRUE, hover = TRUE), 
-      nodesIdSelection = FALSE) |>
+    list(nodes = nodes_df, edges = edges_df)
+  })
+  
+  output$int_network <- renderVisNetwork({
+    net2 <- network2()
+    nodes <- net2$nodes
+    edges <- net2$edges 
     
-    visInteraction(
-      dragNodes = TRUE, 
-      dragView = TRUE, 
-      zoomView = TRUE) |> 
     
-    visPhysics(stabilization = TRUE)
+    visNetwork(nodes, edges) |> 
+      
+      visNodes(borderWidth = 1, 
+               color = list(
+                 background= NULL, 
+                 border = "red", 
+                 highlight =  "purple"))|>
+      
+      visEdges(
+        color = list(color = "purple", highlight = "black")) |> 
+      
+      visOptions(
+        highlightNearest = list(enabled = TRUE, hover = TRUE), 
+        nodesIdSelection = FALSE) |>
+      
+      visInteraction(
+        dragNodes = TRUE, 
+        dragView = TRUE, 
+        zoomView = TRUE) |> 
+      
+      visPhysics(stabilization = TRUE)
     
-})
-
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
-
-
